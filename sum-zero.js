3cf247983017a92c56bde7b82bec5d1d8a6b73de
DebasisSaikia <debasishsaikia136@gmail.com>
2022-06-15 17:41:56 +0530

    sum of zero

--- sum-zero.js
+++ sum-zero.js
@@ -4,14 +4,29 @@ const arr=[-5,-4,-3,-2,0,2,4,6,8]
 
 function sumZero(n){
     
-    for(let nums of n){
-        for(let j=1;j<n.length;j++){
-            console.log('nums n', nums+ n[j])
-            if(nums+n[j]===0){
-                return [nums,n[j]]
-            }
+    //Quadratic o(n~2)
+    // for(let nums of n){
+    //     for(let j=1;j<n.length;j++){
+    //         if(nums+n[j]===0){
+    //             return [nums,n[j]]
+    //         }
+    //     }
+    // }
+
+    //optimized --linear O(n)
+    let left=0;
+    let right=n.length-1;
+    while(left<right){
+        sum=n[left]+n[right]
+        if(sum===0){
+            return [n[left],n[right]]
+        }else if(sum>0){
+            right--
+        }else{
+            left++
         }
     }
+    
 }
 
 console.log(sumZero(arr))
